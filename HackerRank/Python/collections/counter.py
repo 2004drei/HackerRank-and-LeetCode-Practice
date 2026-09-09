7c07710131e3941a2bf76dc3b923297a702e5571
Andrei T. <138087405+2004drei@users.noreply.github.com>
2026-09-10 00:12:18 +0800

    Finished the counter task for Collections

--- HackerRank/Python/collections/counter.py
+++ HackerRank/Python/collections/counter.py
@@ -3,7 +3,15 @@ import collections
 num_of_shoes = int(input())
 sizes_list = collections.Counter(list(map(int, input().split())))
 num_of_customers = int(input())
-
-for i in range(num_of_customers):
+total = 0
+for _ in range(num_of_customers):
   size, price = input().split()
-  if sizes_list[int(size)] > 1:
+  s = int(size)
+  p = int(price)
+  if sizes_list[s] > 0:
+    sizes_list[s] -= 1
+    total += p
+  else:
+    continue
+
+print(total)
